e543d24ee5f8e7c759bf4af1445499c5727b4c1d
cristinamaze <76184003+cristinamaze@users.noreply.github.com>
2021-03-06 13:13:03 -0500

    .ajax API

--- assests/script.js
+++ assests/script.js
@@ -1,4 +1,27 @@
-var SearchFormEL = document.querySelector("#search-form");
+$(document).ready(function() {
+
+    var SearchFormEL = document.querySelector("#search-form");
+
+    // var searchButton = $("#search-form")
+    // searchButton.click(searchFunction)
+
+    // function searchFunction(event) {
+    //     event.preventDefault()
+    //     var zipCode = ""
+    //     console.log(searchCity)
+
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "" + zipCode + "",
+    //         dataType: "json",
+    //         success: function(data) {
+    //             console.log(data)
+
+    //         }
+
+    //     })
+    // }
+// })
 
 function handleSearchFormSubmit (event) {
 event.preventDefault();
@@ -15,4 +38,3 @@ var queryString = './search-results.html?q=' + searchInputVal + '&format=' + for
   location.assign(queryString);
 
 };
-
