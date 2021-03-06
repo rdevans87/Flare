1bc0382de09c3482eef80e98b37a0d339f53e20e
Ryan Evans <rdevans87@gmail.com>
2021-03-06 12:46:32 -0500

    working on search param in js with google search API

--- assests/script.js
+++ assests/script.js
@@ -1,7 +1,18 @@
-var SearchFormEL = document.querySelector("#signup-form");
+var SearchFormEL = document.querySelector("#search-form");
 
-function.handleSearchFormSubmit (event) {
+function handleSearchFormSubmit (event) {
 event.preventDefault();
+
+var searchInputVal = document.querySelector('#search-input').value;
+
+if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+  location.assign(queryString);
 
 };
 
